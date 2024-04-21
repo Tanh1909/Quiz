615e58c675c6f7866cd27275b926ebe18382a2b1
Tanh1909 <dtienanh1909@gmail.com>
2024-04-21 17:01:37 +0700

    add 404 page

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,5 +1,27 @@
+import { Button, Result } from "antd";
+import { useNavigate } from "react-router-dom";
+
 function NotFound() {
-  return <>NotFound</>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button
+            onClick={() => {
+              navigate("/");
+            }}
+            type="primary"
+          >
+            Back Home
+          </Button>
+        }
+      />
+    </>
+  );
 }
 
 export default NotFound;
